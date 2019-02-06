7e8ac6f159628f7f4015f5774c2fef20aa19b585
morwen80 <ribesnero80@gmail.com>
2019-02-06 12:07:21 +0000

    Pokemon cards rendered

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,33 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+const showTrainerCards = document.querySelector('#show-teams')
+
+const fetchTrainers = () => {
+  return fetch(TRAINERS_URL)
+    .then(res => res.json())
+}
+
+const renderAllTrainerCards = (trainers) => {
+  trainers.forEach(renderSingleTrainerCard)
+}
+
+
+const renderSingleTrainerCard = (trainer) => {
+  const pokeList = trainer.pokemons.map(pokemon => `<li>${pokemon.nickname} (${pokemon.species})<button class="release" data-pokemon-id="${pokemon.id}">Release</button></li>`).join(" ")
+
+  const singleCard = document.createElement('div')
+    singleCard.innerHTML = `
+    <div class="card" data-id="${trainer.id}"><p>${trainer.name}</p>
+    <button data-trainer-id="${trainer.id}">Add Pokemon</button>
+    ${pokeList}
+    </div>
+    `
+showTrainerCards.appendChild(singleCard)
+}
+
+
+
+fetchTrainers()
+  .then(renderAllTrainerCards)
